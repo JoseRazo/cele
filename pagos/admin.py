20c40f40e8494262d1952f5abeac273182aa9cc3
Jose Razo <jrazo@utsalamanca.edu.mx>
2023-03-28 10:00:16 -0600

    asignacion de permisos al modulo de pagos

--- pagos/admin.py
+++ pagos/admin.py
@@ -12,14 +12,14 @@ class ReferenciasBancoAdmin(admin.ModelAdmin):
     # fields = ('nombre', 'matricula', 'concepto', 'referencia', 'banco_emisor', 'servicio_bb')
     list_per_page = 20
 
-    def has_add_permission(self, request, obj=None):
-        return False
+    # def has_add_permission(self, request, obj=None):
+    #     return False
 
-    def has_delete_permission(self, request, obj=None):
-        return False
+    # def has_delete_permission(self, request, obj=None):
+    #     return False
 
-    def has_change_permission(self, request, obj=None):
-        return False
+    # def has_change_permission(self, request, obj=None):
+    #     return False
 
     def get_queryset(self, request):
         qs = super().get_queryset(request)
@@ -27,7 +27,7 @@ class ReferenciasBancoAdmin(admin.ModelAdmin):
             return qs.filter(Q(servicio_bb='2354'))
         elif request.user.groups.filter(name='Administradores EDCON'):
             return qs.filter(Q(servicio_bb='2355'))
-        return qs
+        return qs.filter(Q(servicio_bb='2354') | Q(servicio_bb='2355'))
     
 class ReferenciasConceptosBancoAdmin(admin.ModelAdmin):
     list_display = ('concepto', 'monto', 'monto_externo', 'fecha_limite', 'fecha_limite_pago', 'activo',)
@@ -35,14 +35,14 @@ class ReferenciasConceptosBancoAdmin(admin.ModelAdmin):
     list_filter = ('activo',)
     search_fields = ('concepto',)
 
-    def has_add_permission(self, request, obj=None):
-            return False
+    # def has_add_permission(self, request, obj=None):
+    #         return False
 
-    def has_delete_permission(self, request, obj=None):
-        return False
+    # def has_delete_permission(self, request, obj=None):
+    #     return False
 
-    def has_change_permission(self, request, obj=None):
-        return True
+    # def has_change_permission(self, request, obj=None):
+    #     return True
 
     def get_queryset(self, request):
         qs = super().get_queryset(request)
@@ -50,7 +50,7 @@ class ReferenciasConceptosBancoAdmin(admin.ModelAdmin):
             return qs.filter(Q(cve_servicio='2'))
         elif request.user.groups.filter(name='Administradores EDCON'):
             return qs.filter(Q(cve_servicio='3'))
-        return qs
+        return qs.filter(Q(cve_servicio='2') | Q(cve_servicio='3'))
 
 
 
