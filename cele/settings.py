6b0911834a0effc7bb7327e70e80ec3b4937eb4f
Jose Razo <joserrp.13@gmail.com>
2023-05-02 11:02:35 -0600

    se elimino la linea --insecure en docker compose

--- cele/settings.py
+++ cele/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DJANGO_DEBUG', default=False)
+DJANGO_DEBUG = env.bool('DJANGO_DEBUG', default=False)
 
 ALLOWED_HOSTS = tuple(env.list('ALLOWED_HOSTS', default=[]))
 
@@ -96,45 +96,45 @@ WSGI_APPLICATION = 'cele.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'mssql',
-        'HOST': os.environ.get('DB_HOST_EDCON'),
-        'NAME': os.environ.get('DB_NAME_EDCON'),
-        'USER': os.environ.get('DB_USER_EDCON'),
-        'PASSWORD': os.environ.get('DB_PASS_EDCON'),
-        'PORT': os.environ.get('DB_PORT_EDCON'),
-        'OPTIONS': {
-            'driver': 'FreeTDS',
-            'unicode_results': True,
-            'host_is_server': True,
-            'driver_supports_utf8': True,
-            'extra_params': 'tds_version=7.4',
-        }
-    },
-    'sito': {
-        'ENGINE': 'mssql',
-        'HOST': os.environ.get('DB_HOST_SITO'),
-        'NAME': os.environ.get('DB_NAME_SITO'),
-        'USER': os.environ.get('DB_USER_SITO'),
-        'PASSWORD': os.environ.get('DB_PASS_SITO'),
-        'PORT': os.environ.get('DB_PORT_SITO'),
-        'OPTIONS': {
-            'driver': 'FreeTDS',
-            'unicode_results': True,
-            'host_is_server': True,
-            'driver_supports_utf8': True,
-            'extra_params': 'tds_version=7.4',
-        }
-    },
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
+#         'ENGINE': 'mssql',
+#         'HOST': os.environ.get('DB_HOST_EDCON'),
+#         'NAME': os.environ.get('DB_NAME_EDCON'),
+#         'USER': os.environ.get('DB_USER_EDCON'),
+#         'PASSWORD': os.environ.get('DB_PASS_EDCON'),
+#         'PORT': os.environ.get('DB_PORT_EDCON'),
+#         'OPTIONS': {
+#             'driver': 'FreeTDS',
+#             'unicode_results': True,
+#             'host_is_server': True,
+#             'driver_supports_utf8': True,
+#             'extra_params': 'tds_version=7.4',
+#         }
+#     },
+#     'sito': {
+#         'ENGINE': 'mssql',
+#         'HOST': os.environ.get('DB_HOST_SITO'),
+#         'NAME': os.environ.get('DB_NAME_SITO'),
+#         'USER': os.environ.get('DB_USER_SITO'),
+#         'PASSWORD': os.environ.get('DB_PASS_SITO'),
+#         'PORT': os.environ.get('DB_PORT_SITO'),
+#         'OPTIONS': {
+#             'driver': 'FreeTDS',
+#             'unicode_results': True,
+#             'host_is_server': True,
+#             'driver_supports_utf8': True,
+#             'extra_params': 'tds_version=7.4',
+#         }
+#     },
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
@@ -218,14 +218,14 @@ JAZZMIN_UI_TWEAKS = {
 DATABASE_ROUTERS = ['routers.db_routers.AuthRouter',]
 
 #SMTP OFFICE 365
-EMAIL_HOST = env('EMAIL_HOST')
-EMAIL_PORT = env('EMAIL_PORT')
-EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-SERVER_EMAIL = EMAIL_HOST_USER
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = env('EMAIL_USE_TLS')
-EMAIL_TIMEOUT = os.getenv("APP_EMAIL_TIMEOUT", 60)
+# EMAIL_HOST = env('EMAIL_HOST')
+# EMAIL_PORT = env('EMAIL_PORT')
+# EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+# SERVER_EMAIL = EMAIL_HOST_USER
+# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+# EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+# EMAIL_USE_TLS = env('EMAIL_USE_TLS')
+# EMAIL_TIMEOUT = os.getenv("APP_EMAIL_TIMEOUT", 60)
 
 #SMTP GMAIL
 # EMAIL_BACKEND = env('EMAIL_BACKEND')
